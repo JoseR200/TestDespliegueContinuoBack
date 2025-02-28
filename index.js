9c937087ae93825cc71dc1321f7e929b8bf98985
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2025-03-01 00:02:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/users", UserRoutes);
 
 app.get("/test-route", (_req, res) => {
     return res.status(200).json({
-        "version": "3.6.4"
+        "version": "4.4.4"
     });
 });
 
