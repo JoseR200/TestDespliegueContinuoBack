82992fd949da04828e41f58fac483aac4c32cafc
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2025-02-28 18:32:51 +0100

    Test Webhook

--- index.js
+++ index.js
@@ -22,6 +22,31 @@ app.get("/test-route", (_req, res) => {
     });
 });
 
+const secret = 'miSecreto123';
+
+app.post('/webhook', (req, res) => {
+    const signature = req.headers['x-hub-signature-256'];
+    const hash = `sha256=${crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex')}`;
+    
+    if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(hash))) {
+        const ref = req.body.ref;
+
+        if (ref === 'refs/heads/main') {
+            exec('sh /home/bitnami/deploy.sh', (error, _stdout, _stderr) => {
+                if (error) {
+                    console.error(`Error al ejecutar el script: ${error}`);
+                    return res.status(500).send('Error en el despliegue.');
+                }
+                res.status(200).send('Despliegue exitoso.');
+            });
+        } else {
+            res.status(200).send('No se desplegará, ya que no fue un push a main o master.');
+        }
+    } else {
+        res.status(401).send('No autorizado.');
+    }
+});
+
 app.listen(port, () => {
     console.log("Node server running in port:", port); 
 });
